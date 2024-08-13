1881de61278093dd44518957d38c096f3074e570
XL-Classes <xlclasses7@gmail.com>
2024-08-13 21:50:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+let rollNumber = prompt("Enter your roll number here : ");
+if (rollNumber == 101) {
+    alert("Welcome Aditya Kumawat, You have scored 10/10 in PRAYAS - 04");
+}
+else if (rollNumber == 102) {
+    alert("Welcome Aditya Kumawat, You have scored 10/10 in PRAYAS - 04");
+}
+else if (rollNumber == 103) {
+    alert("Welcome Axmita Kumawat, You have scored 5/10 in PRAYAS - 04");
+}
+else if (rollNumber == 104) {
+    alert("Welcome Gaurav Khandekar, You have scored 8/10 in PRAYAS - 04");
+}
+else if (rollNumber == 105) {
+    alert("Welcome Harshit Jangir, You have scored 5/10 in PRAYAS - 04");
+}
+else if (rollNumber == 106) {
+    alert("Welcome Ishan Ali, You have scored 4/10 in PRAYAS - 04");
+}
+else if (rollNumber == 107) {
+    alert("Welcome Lakshita Kumawat, You have scored 10/10 in PRAYAS - 04");
+}
+else if (rollNumber == 108) {
+    alert("Welcome Love Soni, You have scored 9/10 in PRAYAS - 04");
+}
+else if (rollNumber == 109) {
+    alert("Welcome Manish Verma, You have scored 10/10 in PRAYAS - 04");
+}
+else if (rollNumber == 1010) {
+    alert("Welcome Neetin Kumawat, You have scored 10/10 in PRAYAS - 04");
+}
+else if (rollNumber == 1011) {
+    alert("Welcome Nimisha Sharma, You have scored 5/10 in PRAYAS - 04");
+}
+else if (rollNumber == 1012) {
+    alert("Welcome Rohit Kumawat, You have scored 5/10 in PRAYAS - 04");
+}
+else if (rollNumber == 1013) {
+    alert("Welcome Savesh Khan, You have scored 4/10 in PRAYAS - 04");
+}
+else if (rollNumber == 1014) {
+    alert("Welcome Veer Bhargav, You have scored 7/10 in PRAYAS - 04");
+}
+else if (rollNumber == 1015) {
+    alert("Welcome Wahid Hussain, You have scored 7/10 in PRAYAS - 04");
+}
+else{
+    alert("Incorrect Roll Number ! Please Enter Correct Roll Number.")
+}
